459cbdd331a0a8b5ff1bbbad3b28d0adf669629b
smaranda-perju <61982588+smaranda-perju@users.noreply.github.com>
2021-06-06 17:23:16 +0300

    renamed aut.js to auth.js

--- FrontEnd/components/aut.js
+++
@@ -1,9 +0,0 @@
-function isLoggedIn() {
-    let token = localStorage.getItem("token");
-    return token !== null && token !== undefined;
-  }
-  
-  function logout() {
-    localStorage.clear("token");
-    location.href = "";
-  }
---
+++ FrontEnd/components/auth.js
@@ -0,0 +1,9 @@
+function isLoggedIn() {
+  let token = localStorage.getItem("token");
+  return token !== null && token !== undefined;
+}
+
+function logout() {
+  localStorage.clear("token");
+  location.href = "";
+}
